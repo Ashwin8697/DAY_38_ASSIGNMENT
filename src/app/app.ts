e16148cce3125c668cc53ca5a047858f8b24ca3d
Ashwin8697 <ashwin869raj@gmail.com>
2026-06-03 14:26:11 +0530

    Complete Angular course management assignment

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,39 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { CourseList } from './course-list/course-list';
+import { CourseDetail } from './course-detail/course-detail';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CourseList, CourseDetail],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('edulearn');
+
+  courses = [
+    {
+      id: 1,
+      title: 'Angular Basics',
+      description: 'Learn Angular Fundamentals',
+      duration: '20 Hours'
+    },
+    {
+      id: 2,
+      title: 'React Basics',
+      description: 'Learn React Fundamentals',
+      duration: '18 Hours'
+    },
+    {
+      id: 3,
+      title: 'Node JS',
+      description: 'Backend Development',
+      duration: '25 Hours'
+    }
+  ];
+
+  selectedCourse = this.courses[0];
+
+  selectCourse(course: any) {
+    this.selectedCourse = course;
+  }
 }
